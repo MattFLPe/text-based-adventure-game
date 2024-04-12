c6b3a833da73c6ac41c4b4e20b841cca1d7933ae
Mateus Felipe <brasil.mateus748@gmail.com>
2024-04-12 06:21:33 -0300

    Adjust some minor text details

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the text adventure game!");
-        System.out.println();
         startGame();
         clear();
     }
 
 
-    public static void startMenu() {
+    public static void restartMenu() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Start The Game");
+        System.out.println("1. Restart The Game");
         System.out.println("2. End The Game");
         System.out.print("Enter Your Choice: ");
 
         int choice = scanner.nextInt();
 
         if (choice == 1) {
+            score = 0;
             startGame();
         } else {
             System.out.println("Game Over!");
@@ -35,7 +35,7 @@ public class Main {
     }
     private static void startGame() {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println();
         System.out.println("You wake up in a bleak scenario. What will you do?: ");
         System.out.println("1. Explore the bleak scenario");
         System.out.println("2. Return home");
@@ -58,7 +58,7 @@ public class Main {
     }
 
     public static void exploreScenario() {
-        System.out.println("You randomly walk to a lit area and find a open bunker. What will you do?");
+        System.out.println("You randomly walk to a lit area and find an open bunker. What will you do?");
         System.out.println("1. Enter the bunker");
         System.out.println("2. Ignore the bunker");
         System.out.print("Enter Your Choice: ");
@@ -66,7 +66,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int choice = scanner.nextInt();
-        System.out.println();
         System.out.println();
 
         if (choice == 1) {
@@ -76,12 +75,12 @@ public class Main {
                 System.out.println("You die on the spot from bloodloss and infection.");
                 System.out.println("Your score: " + score);
                 System.out.println("Thanks for playing!");
-                pause();
-                startMenu();
+                clear();
+                restartMenu();
             } else {
                 System.out.println("You enter the bunker and find a revolver with one remaining bullet.");
                 score = score + 50;
-                System.out.println("Your score: " + score);
+                System.out.println("Your score (+50 for finding the .357 Magnum): " + score);
                 System.out.println("What's your next move?");
                 insideBunker();
             }
@@ -107,10 +106,8 @@ public class Main {
     public static void returnHome() {
         System.out.println("You come to the conclusion that an adventure is not worth the risk and return home.");
         System.out.println("Game over!");
-        System.out.println();
-        System.out.println();
         clear();
-        startMenu();
+        restartMenu();
     }
 
     public static void insideBunker() {
@@ -126,25 +123,23 @@ public class Main {
         if (choice == 1) {
             System.out.println("You exit the bunker hastily, but end up getting surprised by a corpse.");
             System.out.println("Luckily, you have a newly acquired .357 Magnum loaded with a bullet.");
-            System.out.println("You slay the beast with a clear shot through it's head!");
-            score = score + 50;
-            System.out.println("Your score: " + score);
+            System.out.println("You slay the beast with a clear shot through its head!");
+            score = score + 200;
+            System.out.println("Your score (+200 for slaying the corpse in the most badass way): " + score);
             System.out.println("You walk down a dirt road and find an abandoned house.");
             System.out.println("After a lengthy scan, you discover that there is no threat in the house. You're safe for now.");
-            System.out.println(); System.out.println();
             System.out.println("Thanks for playing!");
             clear();
-            startMenu();
+            restartMenu();
         } else if (choice == 2) {
             System.out.println("You decide to look for supplies down there.");
             System.out.println("Unfortunately, it seems like the place was already ransacked.");
-            System.out.println("A corpse jumps from a metal cabinet behind you and bites your neck.");
+            System.out.println("A corpse jumps from inside a metal cabinet behind you and bites your neck.");
             System.out.println("Just like that, you perish.");
             System.out.println("Your score: " + score);
-            System.out.println(); System.out.println();
             System.out.println("Thanks for playing!");
             clear();
-            startMenu();
+            restartMenu();
         } else {
             System.out.println("Invalid choice. Game over!");
         }
@@ -169,27 +164,28 @@ public class Main {
                 System.out.println("Your screams are muffled while you are eaten alive.");
                 System.out.println("Your score: " + score);
                 System.out.println("Thanks for playing!");
-                pause();
-                startMenu();
+                clear();
+                restartMenu();
             } else {
-                System.out.println("You're able to overpower the creature, pushing it to the ground.");
+                System.out.println("You're able to momentarily overpower the creature, pushing it against to the ground.");
                 System.out.println("You pick up the heaviest object from the living room and drop it on the beast's head.");
                 System.out.println("You killed it! And it looks likes the house is clean. You're safe for now...");
                 score = score + 100;
-                System.out.println("Your score: " + score);
+                System.out.println("Your score (+100 for killing the corpse): " + score);
                 System.out.println("Thanks for playing!");
-                pause();
-                startMenu();
+                System.out.println();
+                clear();
+                restartMenu();
             }
         } else if (choice == 2) {
-            System.out.println("You run! Like your life depended on it (It literally does!).");
-            System.out.println("The beast tries to follow you, but ends up tripping over and falling.");
-            System.out.println("As anticlimactically as it looks, you simply take a knife from the kitchen and stab the fallen beast.");
-            score = score + 100;
-            System.out.println("Your score: " + score);
+            System.out.println("You run! Like your life depended on it (it literally does!).");
+            System.out.println("The beast tries to chase you, but ends up tripping over itself and falling.");
+            System.out.println("As anticlimactic as it sounds, you simply grab a knife from the kitchen and stab the fallen beast.");
+            score = score + 50;
+            System.out.println("Your score (+50 for killing the corpse anticlimactically): " + score);
             System.out.println("Thanks for playing!");
-            pause();
-            startMenu();
+            clear();
+            restartMenu();
         }
         scanner.close();
     }
