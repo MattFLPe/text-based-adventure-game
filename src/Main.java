75d1e9c4bf164cdd54aa49414c396ef9f8d4144d
Mateus Felipe <brasil.mateus748@gmail.com>
2024-04-12 03:30:39 -0300

    Create exploreScenario() function, which gives a player two choices with different outcomes

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,27 @@ public class Main {
         startGame();
         //clear();
     }
+    /*
+    public static void confir()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Start The Game");
+        System.out.println("2. End The Game");
+        System.out.print("Enter Your Choice: ");
 
+        int choice = scanner.nextInt();
+
+        if(choice == 1) {
+            startGame();
+        } else {
+            System.out.println();
+            System.out.println("Game Over!");
+            System.out.println("Your Health: "+ health);
+            System.out.println("Your Score: "+ score);
+        }
+        pause();
+        clear();
+        */
     private static void startGame() {
         Scanner scanner = new Scanner(System.in);
 
@@ -42,12 +62,59 @@ public class Main {
         System.out.println("2. Ignore the bunker");
         System.out.print("Enter Your Choice: ");
 
+        Scanner scanner = new Scanner(System.in);
 
+        int choice = scanner.nextInt();
+        System.out.println(); System.out.println();
+
+        if (choice == 1) {
+            int random = exploreRandom();
+            if (random == 0) {
+                System.out.println("You enter the bunker and a rotting creature bites your neck.");
+                System.out.println("You die on the spot from bloodloss.");
+                System.out.println("Your score: " + score);
+                pause();
+            } else {
+                System.out.println("You enter the bunker and find a revolver with one remaining bullet.");
+                score = score + 50;
+                System.out.println("Your score: " + score);
+                System.out.println("What's your next move?");
+                insideBunker();
+            }
+        } else if (choice == 2) {
+            System.out.println("You ignore the bunker and keep walking.");
+            System.out.println("Soon enough you get surrounded by rotting monsters, but luckily you find a house");
+            System.out.println("Turns out the house isn't so safe. A corpse surprises you!");
+        } else {
+            System.out.println("Invalid choice. Game over!");
+        }
     }
     public static void returnHome() {
         System.out.println("You come to the conclusion that an adventure is not worth the risk and return home.");
         System.out.println(); System.out.println();
         System.out.println("Game over!");
-
     }
+
+    public static void insideBunker() {
+        System.out.println("1. Exit the bunker.");
+        System.out.println("2. Look for more supplies inside the bunker.");
+        System.out.print("Enter Your Choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice = scanner.nextInt();
+        System.out.println();
+
+        if (choice == 1) {
+        System.out.println("You exit the bunker hastily, but end up getting surprised by a corpse.");
+        System.out.println("Luckily, you have a newly acquired .357 Magnum loaded with a bullet.");
+        System.out.println("You slay the beast with a clear shot through it's head!");
+        score = score + 50;
+        System.out.println("Your score: " + score);
+        System.out.println("You walk down a dirt road and find an abandoned house.");
+        System.out.println("After a lengthy scan, you discover that there is no threat in the house. You're safe for now.");
+        pause();
+        clear();
+        confir();
+        }
 }
